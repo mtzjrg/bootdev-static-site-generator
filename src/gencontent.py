5dde0c9b6d6535237850f5c30a48c163c5f205a2
mtzjrg <mtzjrg@proton.me>
2026-01-29 18:07:19 -0600

    add copystatic.py, gencontent.py, test_gencontent.py

---
+++ src/gencontent.py
@@ -0,0 +1,31 @@
+import os
+
+from markdown_blocks import markdown_to_blocks, markdown_to_html_node
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as file:
+        markdown = file.read()
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    title = extract_title(markdown)
+    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+
+    with open(dest_path, "w") as file:
+        file.write(page)
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
